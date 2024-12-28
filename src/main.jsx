a187ac9e56e0a90ad0ddace8b3482419bb3876ee
Stephen Nene <stephen.nene@student.moringaschool.com>
2024-12-28 08:02:57 +0300

    new structure and chcuck

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React,{ StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./assets/styles/index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
+);
